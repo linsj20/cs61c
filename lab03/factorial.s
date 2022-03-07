f02901fc5447dea3373d165a9735b19b097abc87
linsj20 <1093801202@qq.com>
2022-03-07 17:14:18 +0800

    lab03,done!

--- lab03/factorial.s
+++ lab03/factorial.s
@@ -1,7 +1,7 @@
 .globl factorial
 
 .data
-n: .word 8
+n: .word 7
 
 .text
 main:
@@ -22,3 +22,16 @@ main:
 
 factorial:
     # YOUR CODE HERE
+	addi t0, x0, 1
+    beq a0, t0, r
+	addi sp, sp, -8
+    sw a0, 0(sp)
+    sw ra, 4(sp)
+    addi a0, a0, -1
+    jal ra, factorial
+    lw a1, 0(sp)
+    lw ra, 4(sp)
+    addi sp, sp, 8
+    mul a0, a0, a1
+r:
+	jr ra
